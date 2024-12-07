40a1132abde10a815ba6b36eed618c307a45d103
Sayan Deb Sarkar <sayandsarkar.1997@gmail.com>
2024-12-06 22:52:41 -0800

    Updated pagination

---
+++ docs/script.js
@@ -0,0 +1,18 @@
+get = id => document.getElementById(id);
+
+function set_slider(root) {
+    const slidesContainer = root.querySelector(".slides-container");
+    const slide = root.querySelector(".slide");
+    const prevButton = root.querySelector(".slide-arrow-prev");
+    const nextButton = root.querySelector(".slide-arrow-next");
+    nextButton.addEventListener("click", (event) => {
+        const slideWidth = slide.clientWidth;
+        slidesContainer.scrollLeft += slideWidth;
+    });
+    prevButton.addEventListener("click", () => {
+        const slideWidth = slide.clientWidth;
+        slidesContainer.scrollLeft -= slideWidth;
+    });
+}
+sliders = document.getElementsByClassName("slider-wrapper")
+for (var i = 0; i < sliders.length; i++) set_slider(sliders[i])
